b45e3317b936b05673f8cd52863a1843b459aab0
Wayne Montague <wayne.montague@gmail.com>
2013-09-26 11:39:03 -0700

    modified index.html to include containers for 'new tweets' link and 'timeline'. modified javascript such that timeline does not update automatically when new tweets are avail.  instead the 'new tweets' link updates with the amount of new tweets.

--- twittler.js
+++ twittler.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
   var $body = $('body');
-  $body.html('');
-
-  // var index = streams.home.length - 1;
+  // $body.html('');
 
   var pollForTweets = function() {
-    var prevLength = 0;
+    var prevLength = 10;
     var compareLength = function() {
       var currLength = streams.home.length;
 
@@ -18,15 +16,29 @@ $(document).ready(function(){
     setInterval(compareLength, 2000);
   };
 
+  var newTweetsCount = 0;
+
+  var updateNewTweetsCount = function(event, currLength, prevLength) {
+     newTweetsCount = newTweetsCount + (currLength - prevLength); 
+     $('#new-tweets-count').text(String(newTweetsNum));
+  };
+
+  var clearNewTweetsCount = function(event) {
+    newTweetsCount = 0;
+  }
+
+
+  
+
   var displayTweet = function(index) {
     var tweet = streams.home[index];
     var $tweet = $('<div></div>');
     $tweet.text('@' + tweet.user + ': ' + tweet.message + tweet.created_at);
-    $body.prepend($tweet);
+    $('.time-line').prepend($tweet);
     // $tweet.appendTo($body);
   };
 
-  var displayTweets = function(event, currLength, prevLength){
+  /* var displayTweets = function(event, currLength, prevLength){
     var delta;
     if (prevLength === 0) {
       delta = 0;
@@ -36,9 +48,17 @@ $(document).ready(function(){
     for (var i = currLength - 1; i >= delta ; i--) {
       displayTweet(i);
     }
-  };
+  }; */
+
+  index = streams.home.length - 1;
+
+  for (var i = index; i > 0; i--) {
+    displayTweet(i);
+  }
+
+
 
   pollForTweets();
-
-  $body.on('newTweets', displayTweets);
+  $body.on('newTweets', updateNewTweetsNum);
+  // $body.on('newTweets', displayTweets);
 });
